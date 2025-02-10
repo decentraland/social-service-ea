1aa605c51c47c80bba42bd6bd726a082ebb2e75d
Kevin Szuchet <kevin.szuchet@decentraland.org>
2025-02-10 11:01:19 -0300

    fix: Compare to prd instead of prod

--- src/adapters/catalyst-client.ts
+++ src/adapters/catalyst-client.ts
@@ -13,7 +13,7 @@ export async function createCatalystClient({
   config
 }: Pick<AppComponents, 'fetcher' | 'config' | 'logs'>): Promise<ICatalystClientComponent> {
   const loadBalancer = await config.requireString('CATALYST_LAMBDAS_URL_LOADBALANCER')
-  const contractNetwork = (await config.getString('ENV')) === 'prod' ? L1_MAINNET : L1_TESTNET
+  const contractNetwork = (await config.getString('ENV')) === 'prd' ? L1_MAINNET : L1_TESTNET
 
   function getLambdasClientOrDefault(lambdasServerUrl?: string): LambdasClient {
     return createLambdasClient({ fetcher, url: lambdasServerUrl ?? loadBalancer })
