4ed6b977b6cea9f00fadd821ebcebff190c549d2
Kevin Szuchet <kevin.szuchet@decentraland.org>
2025-03-19 09:44:42 +0100

    refactor: Use ws instead of isomorphic for testing purposes

--- test/integration/utils/transport.ts
+++ test/integration/utils/transport.ts
@@ -1,4 +1,4 @@
-import WebSocket, { MessageEvent } from 'isomorphic-ws'
+import WebSocket, { MessageEvent } from 'ws'
 import mitt from 'mitt'
 import type { Transport, TransportEvents } from '@dcl/rpc'
 
