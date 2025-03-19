08d705c9cf4a7c91e22c6e97fac44630a27f9a5a
Kevin Szuchet <kevin.szuchet@decentraland.org>
2025-03-19 10:00:34 +0100

    test: Properly mock peers sync and catalyst client

--- test/integration/rpc-server-controller.spec.ts
+++ test/integration/rpc-server-controller.spec.ts
@@ -1,17 +1,21 @@
 import { test } from '../components'
+import { createMockProfile } from '../mocks/profile'
 import { createOrUpsertActiveFriendship, removeFriendship } from './utils/friendships'
 
-test('RPC Server Controller', function ({ components }) {
+test('RPC Server Controller', function ({ components, stubComponents }) {
   beforeAll(async () => {
     await components.rpcClient.connect()
+    stubComponents.peersSynchronizer.syncPeers.resolves()
   })
-
+  
   describe('GetFriends handler', function () {
     it('returns friends list successfully', async () => {
       const { rpcClient, db } = components
       const friendAddress = '0x06b7c9e6aef7f6b6c259831953309f63c59bcfd1'
-      
+      const mockFriendProfile = createMockProfile(friendAddress)
       const id = await createOrUpsertActiveFriendship(db, [rpcClient.authAddress, friendAddress])
+      
+      stubComponents.catalystClient.getProfiles.resolves([mockFriendProfile])
       
       const response = await rpcClient.client.getFriends({
         pagination: {
@@ -28,6 +32,7 @@ test('RPC Server Controller', function ({ components }) {
     
     it('returns empty list when user has no friends', async () => {
       const { rpcClient } = components
+      stubComponents.catalystClient.getProfiles.resolves([])
       
       const response = await rpcClient.client.getFriends({
         pagination: {
@@ -45,11 +50,14 @@ test('RPC Server Controller', function ({ components }) {
       const { rpcClient, db } = components
       const friendAddress = '0x06b7c9e6aef7f6b6c259831953309f63c59bcfd1'
       const mutualFriendAddress = '0x77c4c17331436d3b8798596e3d7c0d8e1b786aa4'
-
+      const mockMutualFriendProfile = createMockProfile(mutualFriendAddress)
+      
+      stubComponents.catalystClient.getProfiles.resolves([mockMutualFriendProfile])
+      
       const id1 = await createOrUpsertActiveFriendship(db, [rpcClient.authAddress, friendAddress])
       const id2 = await createOrUpsertActiveFriendship(db, [rpcClient.authAddress, mutualFriendAddress])
       const id3 = await createOrUpsertActiveFriendship(db, [friendAddress, mutualFriendAddress])
-
+      
       const response = await rpcClient.client.getMutualFriends({
         user: {
           address: friendAddress
@@ -72,8 +80,9 @@ test('RPC Server Controller', function ({ components }) {
     it('returns empty list when no mutual friends exist', async () => {
       const { rpcClient, db } = components
       const friendAddress = '0x06b7c9e6aef7f6b6c259831953309f63c59bcfd1'
-      
       const id = await createOrUpsertActiveFriendship(db, [rpcClient.authAddress, friendAddress])
+      
+      stubComponents.catalystClient.getProfiles.resolves([])
       
       const response = await rpcClient.client.getMutualFriends({
         user: {
