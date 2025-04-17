330c8b9eab3744c53f14429f450609343ace1cb3
LautaroPetaccio <lausuper@gmail.com>
2025-04-17 17:25:27 -0300

    feat: Add the isFriend property to the privacy messages response

--- src/adapters/rpc-server/services/get-private-messages-settings.ts
+++ src/adapters/rpc-server/services/get-private-messages-settings.ts
@@ -7,45 +7,70 @@ import { RpcServerContext, RPCServiceContext } from '../../../types'
 import { convertDBSettingsToRPCSettings } from '../../../logic/settings'
 import { isErrorWithMessage } from '../../../utils/errors'
 
+const MAX_USER_ADDRESSES = 50
+
 export function getPrivateMessagesSettingsService({ components: { logs, db } }: RPCServiceContext<'logs' | 'db'>) {
   const logger = logs.getLogger('get-private-messages-settings-service')
 
   return async function (
     request: GetPrivateMessagesSettingsPayload,
-    _: RpcServerContext
+    context: RpcServerContext
   ): Promise<GetPrivateMessagesSettingsResponse> {
     try {
       const userAddresses = request.user.map((user) => user.address.toLowerCase())
 
-      // Create base message privacy settings map
-      const privacySettings = userAddresses.reduce(
+      if (userAddresses.length > MAX_USER_ADDRESSES) {
+        return {
+          response: {
+            $case: 'invalidRequest',
+            invalidRequest: {
+              message: `Too many user addresses: ${userAddresses.length}`
+            }
+          }
+        }
+      }
+
+      // Create base message privacy information map
+      const privacyInformation = userAddresses.reduce(
         (acc, address) => {
-          acc[address] = PrivateMessagePrivacySetting.ALL
+          acc[address] = { privacy: PrivateMessagePrivacySetting.ALL, isFriend: false }
           return acc
         },
-        {} as Record<string, PrivateMessagePrivacySetting>
+        {} as Record<string, { privacy: PrivateMessagePrivacySetting; isFriend: boolean }>
       )
 
-      const settings = userAddresses.length > 0 ? await db.getSocialSettings(userAddresses) : []
+      const [settings, friendsOfConnectedAddress] =
+        userAddresses.length > 0
+          ? await Promise.all([
+              db.getSocialSettings(userAddresses),
+              db.getFriendsFromList(context.address, userAddresses)
+            ])
+          : [[], []]
 
       settings.forEach((setting) => {
-        privacySettings[setting.address] = convertDBSettingsToRPCSettings(setting).privateMessagesPrivacy
+        privacyInformation[setting.address].privacy = convertDBSettingsToRPCSettings(setting).privateMessagesPrivacy
+      })
+
+      friendsOfConnectedAddress.forEach((friend) => {
+        privacyInformation[friend.address].isFriend = true
       })
 
       return {
         response: {
           $case: 'ok',
           ok: {
-            settings: Object.entries(privacySettings).map(([address, privacy]) => ({
+            settings: Object.entries(privacyInformation).map(([address, { privacy, isFriend }]) => ({
               user: {
                 address
               },
-              privateMessagesPrivacy: privacy
+              privateMessagesPrivacy: privacy,
+              isFriend
             }))
           }
         }
       }
     } catch (error) {
+      console.error('Error getting private messages settings', error)
       const errorMessage = isErrorWithMessage(error) ? error.message : 'Unknown error'
       logger.error(`Error getting private messages settings: ${errorMessage}`)
       return {
