2734e3746dc26c8343915050a5b076a7932872a1
LautaroPetaccio <lausuper@gmail.com>
2025-04-21 11:47:09 -0300

    fix: Build function to process user private messages settings

--- src/adapters/rpc-server/services/get-private-messages-settings.ts
+++ src/adapters/rpc-server/services/get-private-messages-settings.ts
@@ -1,10 +1,9 @@
 import {
   GetPrivateMessagesSettingsPayload,
-  GetPrivateMessagesSettingsResponse,
-  PrivateMessagePrivacySetting
+  GetPrivateMessagesSettingsResponse
 } from '@dcl/protocol/out-js/decentraland/social_service/v2/social_service_v2.gen'
 import { RpcServerContext, RPCServiceContext } from '../../../types'
-import { convertDBSettingsToRPCSettings } from '../../../logic/settings'
+import { buildPrivateMessagesRPCSettingsForAddresses } from '../../../logic/settings'
 import { isErrorWithMessage } from '../../../utils/errors'
 
 const MAX_USER_ADDRESSES = 50
@@ -31,15 +30,6 @@ export function getPrivateMessagesSettingsService({ components: { logs, db } }: 
         }
       }
 
-      // Create base message privacy information map
-      const privacyInformation = userAddresses.reduce(
-        (acc, address) => {
-          acc[address] = { privacy: PrivateMessagePrivacySetting.ALL, isFriend: false }
-          return acc
-        },
-        {} as Record<string, { privacy: PrivateMessagePrivacySetting; isFriend: boolean }>
-      )
-
       const [settings, friendsOfConnectedAddress] =
         userAddresses.length > 0
           ? await Promise.all([
@@ -48,13 +38,11 @@ export function getPrivateMessagesSettingsService({ components: { logs, db } }: 
             ])
           : [[], []]
 
-      settings.forEach((setting) => {
-        privacyInformation[setting.address].privacy = convertDBSettingsToRPCSettings(setting).privateMessagesPrivacy
-      })
-
-      friendsOfConnectedAddress.forEach((friend) => {
-        privacyInformation[friend.address].isFriend = true
-      })
+      const privacyInformation = buildPrivateMessagesRPCSettingsForAddresses(
+        userAddresses,
+        settings,
+        friendsOfConnectedAddress
+      )
 
       return {
         response: {
@@ -71,7 +59,6 @@ export function getPrivateMessagesSettingsService({ components: { logs, db } }: 
         }
       }
     } catch (error) {
-      console.error('Error getting private messages settings', error)
       const errorMessage = isErrorWithMessage(error) ? error.message : 'Unknown error'
       logger.error(`Error getting private messages settings: ${errorMessage}`)
       return {
