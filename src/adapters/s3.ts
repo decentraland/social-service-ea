4fdcec857bc592dd46c58371c3c887feed761b2c
Alejo Thomas Ortega <alejothomasortega@gmail.com>
2025-06-10 09:39:10 -0300

    fix: bucket integration (#144)

--- src/adapters/s3.ts
+++ src/adapters/s3.ts
@@ -4,10 +4,10 @@ import { Upload } from '@aws-sdk/lib-storage'
 import { AppComponents, IStorageComponent } from '../types'
 
 export async function createS3Adapter({ config }: Pick<AppComponents, 'config'>): Promise<IStorageComponent> {
-  const bucket = await config.requireString('BUCKET')
-  const bucketEndpoint = (await config.getString('BUCKET_ENDPOINT')) || 'https://s3.amazonaws.com'
+  const bucket = await config.requireString('AWS_S3_BUCKET')
+  const bucketEndpoint = (await config.getString('AWS_S3_BUCKET_ENDPOINT')) || 'https://s3.amazonaws.com'
   const region = (await config.getString('AWS_REGION')) || 'us-east-1'
-  const bucketPrefix = (await config.getString('BUCKET_PREFIX')) || 'social' // service only has access to social directory
+  const bucketPrefix = (await config.getString('AWS_S3_BUCKET_PREFIX')) || 'social' // service only has access to social directory
 
   const s3 = new S3Client({ region, endpoint: bucketEndpoint })
 
