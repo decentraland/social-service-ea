7a2f0ba0a69010775f320c7fb7daf6e4483b91ed
Braian Mellor <braianj@gmail.com>
2025-06-30 18:16:34 -0300

    feat: merge functions in rewards adapter

--- src/adapters/rewards.ts
+++ src/adapters/rewards.ts
@@ -1,37 +1,32 @@
 import { AppComponents, IRewardComponent } from '../types'
 import { RewardAttributes } from '../logic/referral/types'
 
-export function createRewardComponent(components: Pick<AppComponents, 'fetcher' | 'config'>): IRewardComponent {
+export async function createRewardComponent(
+  components: Pick<AppComponents, 'fetcher' | 'config'>
+): Promise<IRewardComponent> {
   const { fetcher, config } = components
 
-  const fetchJson = async (baseURL: URL, path: string, body: { campaign_key: string; beneficiary: string }) => {
-    let url = baseURL.toString()
-    if (!url.endsWith('/')) {
-      url += '/'
-    }
-    url += path
-    const response = await fetcher.fetch(url, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(body)
-    })
-
-    if (response.ok === true) {
-      return response.json()
-    }
-
-    throw new Error(`Failed to fetch ${url}: ${response.status} ${await response.text()}`)
-  }
+  const rewardUrl = new URL(await config.requireString('REWARD_SERVER_URL'))
 
   async function sendReward(
     campaignKey: string,
     beneficiary: string
   ): Promise<{ ok: boolean; data: RewardAttributes[] }> {
-    const rewardUrl = new URL(await config.requireString('REWARD_SERVER_URL'))
-    return fetchJson(rewardUrl, 'rewards', { campaign_key: campaignKey, beneficiary })
+    rewardUrl.pathname = '/rewards'
+    const response = await fetcher.fetch(rewardUrl.toString(), {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ campaign_key: campaignKey, beneficiary })
+    })
+
+    if (response.ok) {
+      return response.json()
+    }
+
+    throw new Error(`Failed to fetch ${rewardUrl.toString()}: ${response.status} ${await response.text()}`)
   }
 
   return {
