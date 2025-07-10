cc97292d281dd5a2d0fff501b1de16bf75854b50
Alejo Thomas Ortega <alejo.ortega@decentraland.org>
2025-07-10 14:52:19 -0300

    test: expect that CDN Cache Invalidator is called whenever a thumbnail is updated

--- test/integration/update-community-controller.spec.ts
+++ test/integration/update-community-controller.spec.ts
@@ -9,7 +9,7 @@ import {
 import { randomUUID } from 'crypto'
 import FormData from 'form-data'
 
-test('Update Community Controller', async function ({ components, stubComponents }) {
+test('Update Community Controller', async function ({ components, stubComponents, spyComponents }) {
   const makeMultipartRequest = makeAuthenticatedMultipartRequest(components)
   const makeRequest = makeAuthenticatedRequest(components)
 
@@ -275,6 +275,7 @@ test('Update Community Controller', async function ({ components, stubComponents
               expect(body.data.thumbnails).toBeDefined()
               expect(body.data.thumbnails.raw).toContain('social/communities/')
               expect(body.message).toBe('Community updated successfully')
+              expect(spyComponents.cdnCacheInvalidator.invalidateThumbnail).toHaveBeenCalledWith(communityId)
             })
           })
         })
