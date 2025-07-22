0b5ab5eafc356791557a5a12c49baee353a16605
Kevin Szuchet <kevin.szuchet@decentraland.org>
2025-07-22 12:34:40 +0300

    refactor: Get Friendship Status moved to the business logic component

--- src/logic/friends/types.ts
+++ src/logic/friends/types.ts
@@ -2,6 +2,7 @@ import { Profile } from 'dcl-catalyst-client/dist/client/specs/lambdas-client'
 import { EthAddress } from '@dcl/schemas'
 import { Pagination } from '@dcl/protocol/out-js/decentraland/social_service/v2/social_service_v2.gen'
 import { BlockedUserWithDate } from '../../types'
+import { FriendshipStatus } from '@dcl/protocol/out-js/decentraland/social_service/v2/social_service_v2.gen'
 
 export type BlockedUser = {
   profile: Profile
@@ -18,4 +19,5 @@ export interface IFriendsComponent {
     userAddress: string
   ): Promise<{ blockedUsers: BlockedUserWithDate[]; blockedProfiles: Profile[]; total: number }>
   getBlockingStatus(userAddress: string): Promise<{ blockedUsers: string[]; blockedByUsers: string[] }>
+  getFriendshipStatus(loggedUserAddress: string, userAddress: string): Promise<FriendshipStatus>
 }
