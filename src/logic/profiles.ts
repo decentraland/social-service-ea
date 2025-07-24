f84ee9a0338365bbf2dfeadee484fb25d4d6e209
Kevin Szuchet <kevin.szuchet@decentraland.org>
2025-07-24 12:20:49 +0300

    chore: Remove unused import

--- src/logic/profiles.ts
+++ src/logic/profiles.ts
@@ -1,6 +1,5 @@
 import { Profile, ProfileAvatarsItem } from 'dcl-catalyst-client/dist/client/specs/lambdas-client'
 import { normalizeAddress } from '../utils/address'
-import { EthAddress } from '@dcl/schemas'
 
 export function getProfileAvatarItem(profile: Pick<Profile, 'avatars'>): ProfileAvatarsItem {
   const [avatar] = profile.avatars ?? []
