987be062e1f76fb7ff376d63e31b3000b3ec63bd
Alejo Thomas Ortega <alejo.ortega@decentraland.org>
2025-08-05 13:58:38 -0300

    chore: remove unused import

--- src/controllers/handlers/http/update-community-handler.ts
+++ src/controllers/handlers/http/update-community-handler.ts
@@ -2,7 +2,7 @@ import { DecentralandSignatureContext } from '@dcl/platform-crypto-middleware'
 import { FormHandlerContextWithPath, HTTPResponse } from '../../../types/http'
 import { InvalidRequestError, NotAuthorizedError } from '@dcl/platform-server-commons'
 import { errorMessageOrDefault } from '../../../utils/errors'
-import { CommunityNotFoundError, CommunityPrivacyEnum } from '../../../logic/community'
+import { CommunityNotFoundError } from '../../../logic/community'
 import { validateCommunityFields } from '../../../utils/community-validation'
 
 export async function updateCommunityHandler(
