045b6c9fe3643f838d9a0838d585a8c561b5f24b
Braian Mellor <braianj@gmail.com>
2025-08-14 11:35:28 -0300

    feat: remove finalize referral

--- src/logic/sqs/message-processor.ts
+++ src/logic/sqs/message-processor.ts
@@ -22,7 +22,7 @@ export async function createMessageProcessorComponent({
       return
     }
 
-    await referral.finalizeReferral(userAddress)
+    // await referral.finalizeReferral(userAddress)
 
     return
   }
