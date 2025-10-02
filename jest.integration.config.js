86d3084acd60c31ff08dbb84847fd35b98205096
Kevin Szuchet <kevin.szuchet@decentraland.org>
2025-10-02 12:00:31 +0300

    chore: Remove redundant prop in jest integration config

--- jest.integration.config.js
+++ jest.integration.config.js
@@ -9,7 +9,5 @@ module.exports = {
   testMatch: ['**/test/integration/**/*.spec.(ts)'],
   testEnvironment: 'node',
   setupFilesAfterEnv: ['<rootDir>/test/integration/setupTests.ts'],
-  resetMocks: true,
-  // Force tests to run sequentially to avoid port conflicts
-  maxWorkers: 1
+  resetMocks: true
 }
